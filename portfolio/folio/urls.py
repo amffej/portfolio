e48b5912680e4b5026623609a179be126fb57b3c
Jefferson Maldonado <jeffmaldo27@gmail.com>
2019-08-06 20:01:07 -0500

    updated folio summary detailed page

--- portfolio/folio/urls.py
+++ portfolio/folio/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("portfolio/<int:item_id>", views.folio, name='folio'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('fadmin/', views.admin, name='admin'),
--- portfolio/folio/views.py
+++ portfolio/folio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from . models import PortfolioEntry
 
 
@@ -8,6 +8,17 @@ def index(request):
         "portfolio_entries": PortfolioEntry.objects.all()
     }
     return render(request, "folio/index.html", context)
+
+def folio(request, item_id):
+    try:
+        entry = PortfolioEntry.objects.get(pk=item_id)
+    except entry.DoesNotExist:
+        raise Http404("Entree does not exist")
+    context = {
+        "entry": entry
+    }
+    return render(request, "folio/folio.html", context)
+
 
 def contact(request):
     context = {
